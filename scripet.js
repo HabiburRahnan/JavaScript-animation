756b5ae81a2093f8672ffe90c48b574b57a9e67d
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-07-16 04:28:32 +0600

    animation and drop dwon setup and design

--- scripet.js
+++ scripet.js
@@ -1,3 +1,9 @@
+let playerState = "idle"
+const dropdwon = document.getElementById("animation");
+dropdwon.addEventListener("change", function (e) {
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 // console.log(ctx)
@@ -9,16 +15,78 @@ const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
 const spriteWidth = 573;
 const spriteHeigth = 523;
-let frameX = 0;
-let frameY = 0;
 
 
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 1,
+    },
+    {
+        name: "jump",
+        frames: 7,
+    },
+    {
+        name: "fail",
+        frames: 7,
+    },
+    {
+        name: "run",
+        frames: 9,
+    },
+    {
+        name: "dizzy",
+        frames: 11,
+    },
+    {
+        name: "sit",
+        frames: 5,
+    },
+    {
+        name: "roll",
+        frames: 7,
+    },
+    {
+        name: "bite",
+        frames: 7,
+    },
+    {
+        name: "ko",
+        frames: 12,
+    },
+    {
+        name: "getHit",
+        frames: 4,
+    },
+
+
+]
+
+animationStates.forEach((state, index,) => {
+    let frames = {
+        loc: [],
+    };
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeigth;
+        frames.loc.push({ x: positionX, y: positionY })
+    };
+    spriteAnimations[state.name] = frames;
+
+})
+// console.log(spriteAnimations)
 function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH)
-    // ctx.fillRect(x, 50, 100, 100)
-    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeigth, spriteWidth, spriteHeigth, 0, 0, CANVAS_WIDTH, CANVAS_HEIGTH)
-    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteHeigth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y
+
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeigth, 0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
+    gameFrame++;
+    requestAnimationFrame(animate);
 };
 
 animate();
